bca822168f58f232bcf06623a85f980d426b2721
Dieter Castel <dietercastel@gmail.com>
2019-11-26 08:43:05 +0100

    Added qr code and generation script.

---
+++ makeqr.py
@@ -0,0 +1,15 @@
+#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3.6
+import qrcode
+#for i in range(5):
+i=360*360
+qr = qrcode.QRCode(
+    version=1,
+    box_size=10,
+    border=0,
+)
+qr.add_data('https://nofb.github.io')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+print(img)
+img.resize((360,360)).save("mynewface"+str(i)+".png", "PNG")
